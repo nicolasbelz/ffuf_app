7a3fc0b267b664cbd5da35622c779509b5b68975
“Nicolas <nicolasbelz10@gmail.com>
2023-11-12 07:21:24 -0500

    updates - v1

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+# Normal endpoints simulating a typical web application structure.
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        # Process the contact form submission (e.g., save to a database, send email)
+        return "Thank you for contacting us!"
+    return render_template('contact.html')
+
+@app.route('/products/<product_id>')
+def product_detail(product_id):
+    # Retrieve and display details for a product with the given id
+    return render_template('product_detail.html', product_id=product_id)
+
+@app.route('/search')
+def search():
+    query = request.args.get('query', '')
+    # Perform a search and return results
+    return render_template('search_results.html', query=query)
+
+# Fuzzing test endpoints from the original app remain unchanged here.
+
+# Rest of the original fuzzing endpoints...
+
+@app.route('/directory_fuzzing/secret_endpoint/')
+def directory_fuzzing():
+    return "You found the secret endpoint for directory fuzzing!"
+
+@app.route('/page_fuzzing/secret_page.html')
+def page_fuzzing():
+    return "You found the secret page for page fuzzing!"
+
+@app.route('/recursive_fuzzing/level1/level2/secret')
+def recursive_fuzzing():
+    return "You found the secret endpoint for recursive fuzzing!"
+
+@app.route('/parameter_fuzzing/')
+def parameter_fuzzing():
+    secret = request.args.get('secret_param', None)
+    if secret == 'secret_value':
+        return "You found the secret parameter with the correct value!"
+    return "Try fuzzing the parameters!"
+
+@app.route('/value_fuzzing', methods=['POST'])
+def value_fuzzing():
+    if request.form.get('key') == 'secret_value':
+        return "You fuzzed the correct POST value!"
+    return "Try fuzzing POST values!"
+
+# Sub-domain Fuzzing, Vhost Fuzzing, and others would require additional configurations
+# outside of this Flask app.
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)

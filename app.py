2557fd8349f9980837e80101f316283e8ff030f4
“Nicolas <nicolasbelz10@gmail.com>
2023-11-14 21:24:31 -0500

    update v3

--- app.py
+++ app.py
@@ -34,34 +34,38 @@ def search():
 
 # Rest of the original fuzzing endpoints...
 
-@app.route('/directory_fuzzing/')
-def directory_dir_fuzzing():
-    return render_template('directory.html')
+@app.route('/admin/')
+def admin():
+    return render_template('admin.html')
 
-@app.route('/directory_fuzzing/secret_endpoint/')
-def directory_fuzzing():
-    return "You found the secret endpoint for directory fuzzing!"
+@app.route('/admin/users/')
+def admin_users():
+    return render_template('admin_users.html')
 
-@app.route('/page_fuzzing/secret_page.html')
-def page_fuzzing():
-    return "You found the secret page for page fuzzing!"
+@app.route('/admin/users/mails')
+def admin_users_mails():
+    return "hello world"
 
-@app.route('/recursive_fuzzing/level1/level2/secret')
-def recursive_fuzzing():
-    return "You found the secret endpoint for recursive fuzzing!"
+# @app.route('/page_fuzzing/secret_page.html')
+# def page_fuzzing():
+#     return "You found the secret page for page fuzzing!"
 
-@app.route('/parameter_fuzzing/')
-def parameter_fuzzing():
-    secret = request.args.get('secret_param', None)
-    if secret == 'secret_value':
-        return "You found the secret parameter with the correct value!"
-    return "Try fuzzing the parameters!"
+# @app.route('/recursive_fuzzing/level1/level2/secret')
+# def recursive_fuzzing():
+#     return "You found the secret endpoint for recursive fuzzing!"
 
-@app.route('/value_fuzzing', methods=['POST'])
-def value_fuzzing():
-    if request.form.get('key') == 'secret_value':
-        return "You fuzzed the correct POST value!"
-    return "Try fuzzing POST values!"
+# @app.route('/parameter_fuzzing/')
+# def parameter_fuzzing():
+#     secret = request.args.get('secret_param', None)
+#     if secret == 'secret_value':
+#         return "You found the secret parameter with the correct value!"
+#     return "Try fuzzing the parameters!"
+
+# @app.route('/value_fuzzing', methods=['POST'])
+# def value_fuzzing():
+#     if request.form.get('key') == 'secret_value':
+#         return "You fuzzed the correct POST value!"
+#     return "Try fuzzing POST values!"
 
 # Sub-domain Fuzzing, Vhost Fuzzing, and others would require additional configurations
 # outside of this Flask app.
